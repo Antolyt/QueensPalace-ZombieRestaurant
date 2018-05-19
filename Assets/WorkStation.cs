2fd65f064915e06611659eee0fb032aca42e641c
Julian Benda <Zabuza98@gmx.net>
2018-05-18 20:42:57 -0700

    add herd colider

---
+++ Assets/WorkStation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkStation : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        this.GetComponent<MeshRenderer>().material.color = Color.red;
+        Debug.Log("hit");
+    }
+    void OnTriggerExit(Collider other) {
+        this.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Create");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
