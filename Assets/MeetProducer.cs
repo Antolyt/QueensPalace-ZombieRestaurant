96e3033812392b93d02e5a418b0df5551ad5c257
Julian Benda <Zabuza98@gmx.net>
2018-05-18 21:46:38 -0700

    workstation can Produce and Carry

---
+++ Assets/MeetProducer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeetProducer : MonoBehaviour {
+    private CarrayObject _myCarry;
+    private Food _head = new Food(Food.BodyPart.HEAD);
+	void Start () {
+        _myCarry = this.GetComponent<CarrayObject>();
+        _myCarry.GiveFood(_head);
+    }
+	void Update () {
+        _myCarry.GiveFood(_head);
+    }
+}
---
+++ Assets/Objects/CarrayObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrayObject : MonoBehaviour {
+
+    private Food _food = null;
+
+    public bool IsEmpty() {
+        return _food == null;
+    }
+    public bool GiveFood(Food food) {
+        if (_food == null)
+        {
+            Debug.Log("getFood");
+            _food = food;
+            return true;
+        }
+        else return false;
+    }
+    public Food GetFood() {
+        Food f = _food;
+        _food = null;
+        return f;
+    }
+
+	void Start () {}
+	void Update () {
+        this.GetComponent<MeshRenderer>().material.color = IsEmpty() ? Color.red : Color.blue;
+    }
+}
