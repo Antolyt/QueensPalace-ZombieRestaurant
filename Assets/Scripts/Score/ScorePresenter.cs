b1a44b0a925ba877cec0efd48d140536c3fa9b6b
Tony Schwensfeier <finalcow@yahoo.de>
2018-05-26 18:47:11 +0200

    feat: Added Main Menu functionality

---
+++ Assets/Scripts/Score/ScorePresenter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ScorePresenter : MonoBehaviour {
+
+    public Text scoreText;
+    public Text gameplayScoreText;
+
+    public int waitTime;
+    private float timeStemp;
+    public UnityEvent action;
+
+    void Start () {
+        scoreText.text = gameplayScoreText.text;
+        timeStemp = Time.time;
+	}
+	
+    /// <summary>
+    /// Go to next UI after input and time offset
+    /// </summary>
+	void Update () {
+        if((Input.GetButtonDown("Fire1") || Input.GetButtonDown("Submit")) && Time.time > timeStemp + waitTime)
+        {
+            if (action != null) action.Invoke();
+            return;
+        }
+	}
+}
